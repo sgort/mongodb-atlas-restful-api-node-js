90424a074bbf7e9b1e081d83c8a5b476f7a8b2ac
Datafluisteraar <steven.gort@ictu.nl>
2020-04-04 22:26:28 +0200

    Intermediate commit

--- app.js
+++ app.js
@@ -2,35 +2,37 @@ const Express = require("express");
 const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
+const Morgan = require("morgan");
 
+const gemeenteRoutes = require("./api/routes/gemeente");
 
 const CONNECTION_URL = "mongodb+srv://dbUser:" +
     process.env.MONGO_ATLAS_PW +
     "@disciplmongodb-wc0s0.mongodb.net/test?retryWrites=true&w=majority";
 const DATABASE_NAME = "test";
 
-var app = Express();
+const app = Express();
 
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
+app.use(Morgan('dev'));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
     }
     next();
-  });
-  
+});
 
 app.listen(3000, () => {
     MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-        if(error) {
+        if (error) {
             throw error;
         }
         database = client.db(DATABASE_NAME);
@@ -39,20 +41,40 @@ app.listen(3000, () => {
     });
 });
 
-app.post("/person", (req, res) => {
-    collection.insert(req.body, (err, result) => {
-        if(err) {
+// Routes which should handle requests
+app.use("/gemeente", gemeenteRoutes);
+
+app.post("/person", (req, res, next) => {
+    collection.insertOne(req.body, (err, result) => {
+        if (err) {
             return res.status(500).send(err);
         }
-        res.send(res.result);
+        res.send(result.result);
     });
 });
 
-app.get("/people", (req, res, next) => {
-    collection.find({}).toArray((err, result) => {
-        if(err) {
+app.get("/person/:id", (req, res, next) => {
+    collection.findOne({ "_id": new ObjectId(req.params.id) }, (err, result) => {
+        if (err) {
             return res.status(500).send(err);
         }
         res.send(result);
     });
 });
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+module.exports = app;
