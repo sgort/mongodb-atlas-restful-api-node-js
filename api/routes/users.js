2b906e6d5d2b950e057b9c18bb1463ca978318ce
Datafluisteraar <steven.gort@ictu.nl>
2020-04-08 17:26:55 +0200

    Adding user route & signup (ie creating users)

---
+++ api/routes/users.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+
+/**
+ * GET (ie READ) all users in the collection
+ */
+router.get("/", (req, res, next) => {
+    User.find()
+        .exec()
+        .then(docs => {
+            const response = {
+                count: docs.length,
+                users: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        email: doc.email,
+                        password: doc.password,
+                        request: {
+                            type: "GET_SPECIFIC_USER (this is not implemented yet)",
+                            url: "http://localhost:3000/users/" + doc._id
+                        }
+                    };
+                })
+            };
+            console.log(docs);
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+
+router.post("/signup", (req, res, next) => {
+    User.find({ email: req.body.email })
+      .exec()
+      .then(user => {
+        if (user.length >= 1) {
+          return res.status(409).json({
+            message: "Mail exists"
+          });
+        } else {
+          bcrypt.hash(req.body.password, 10, (err, hash) => {
+            if (err) {
+              return res.status(500).json({
+                error: err
+              });
+            } else {
+              const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: req.body.email,
+                password: hash
+              });
+              user
+                .save()
+                .then(result => {
+                  console.log(result);
+                  res.status(201).json({
+                    message: "User created"
+                  });
+                })
+                .catch(err => {
+                  console.log(err);
+                  res.status(500).json({
+                    error: err
+                  });
+                });
+            }
+          });
+        }
+      });
+  });
+  
+  router.delete("/:userId", (req, res, next) => {
+    User.remove({ _id: req.params.userId })
+      .exec()
+      .then(result => {
+        res.status(200).json({
+          message: "User deleted"
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
+  
+
+module.exports = router;
+
