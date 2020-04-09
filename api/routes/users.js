05167fb3c5111ebbd97b889d3202142dc2683e9a
Datafluisteraar <steven.gort@ictu.nl>
2020-04-09 18:59:52 +0200

    Added controllers

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,152 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
-const User = require('../models/user');
+const checkAuth = require('../middleware/check-auth');
+const UserController = require('../controllers/user');
 
 /**
- * GET (ie READ) all users in the collection
+ * GET (ie READ) list of all users in the collection
  */
-router.get("/", (req, res, next) => {
-    User.find()
-        .exec()
-        .then(docs => {
-            const response = {
-                count: docs.length,
-                users: docs.map(doc => {
-                    return {
-                        _id: doc._id,
-                        email: doc.email,
-                        password: doc.password,
-                        request: {
-                            type: "GET_SPECIFIC_USER (this is not implemented yet)",
-                            url: "http://localhost:3000/users/" + doc._id
-                        }
-                    };
-                })
-            };
-            console.log(docs);
-            res.status(200).json(response);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
-});
+router.get("/", UserController.user_list);
 
 /**
  * POST (ie CREATE) a new user in the collection
  */
-router.post("/signup", (req, res, next) => {
-    User.find({ email: req.body.email })
-        .exec()
-        .then(user => {
-            if (user.length >= 1) {
-                return res.status(409).json({
-                    message: "Mail exists"
-                });
-            } else {
-                bcrypt.hash(req.body.password, 10, (err, hash) => {
-                    if (err) {
-                        return res.status(500).json({
-                            error: err
-                        });
-                    } else {
-                        const user = new User({
-                            _id: new mongoose.Types.ObjectId(),
-                            email: req.body.email,
-                            password: hash
-                        });
-                        user
-                            .save()
-                            .then(result => {
-                                console.log(result);
-                                res.status(201).json({
-                                    message: "User created"
-                                });
-                            })
-                            .catch(err => {
-                                console.log(err);
-                                res.status(500).json({
-                                    error: err
-                                });
-                            });
-                    }
-                });
-            }
-        });
-});
-
+router.post("/signup", UserController.user_signup);
 
 /**
- * POST (ie CREATE) a token for the athenticated user to actually use the API
+ * POST (ie CREATE) a token for the user to authenticate use of the API's with protected routes
  */
-router.post("/login", (req, res, next) => {
-    User.find({ email: req.body.email })
-        .exec()
-        .then(user => {
-            if (user.length < 1) {
-                return res.status(401).json({
-                    message: "Auth failed"
-                });
-            }
-            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-                if (err) {
-                    return res.status(401).json({
-                        message: "Auth failed"
-                    });
-                }
-                if (result) {
-                    const token = jwt.sign(
-                        {
-                            email: user[0].email,
-                            userId: user[0]._id
-                        },
-                        process.env.JWT_KEY,
-                        {
-                            expiresIn: "1h"
-                        }
-                    );
-                    return res.status(200).json({
-                        message: "Auth successful",
-                        token: token
-                    });
-                }
-                res.status(401).json({
-                    message: "Auth failed"
-                });
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
-});
+router.post("/login", UserController.user_login);
 
 /**
  * DELETE a specific user from the collection
+ * Requires authentication
  */
-router.delete("/:userId", (req, res, next) => {
-    User.remove({ _id: req.params.userId })
-        .exec()
-        .then(result => {
-            res.status(200).json({
-                message: "User deleted"
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
-});
+router.delete("/:userId", checkAuth, UserController.user_delete );
 
 
 module.exports = router;
